086357348084853dc12b53423d2daeaac62972ad
Ashritha Reddy Annadi <annadiashritha.an@gmail.com>
2023-03-26 23:11:44 +0530

    added cart

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,16 @@
 import { Fragment, useContext } from 'react';
 import {Outlet, Link} from 'react-router-dom';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
 import './navigation.styles.scss';
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 const Navigation = () => {
     const {currentUser} = useContext(UserContext);
-
+    const {isCartOpen} = useContext(CartContext);
     return (
       <Fragment>
         <div className='navigation'>
@@ -25,8 +28,9 @@ const Navigation = () => {
               SIGN-IN
             </Link>)
             }
-           
+           <CartIcon/>
           </div>
+          {isCartOpen && <CartDropdown/> }
         </div>
         <Outlet/>
       </Fragment>
